23dee273f9472ebcad451f2585214265ebb5ded8
DiegoloM2 <diego.moreno.manes@hotmail.com>
2022-05-31 21:01:14 +0200

    Created custom user and custom user manager

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+
+    #Local Apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres', 
+        'USER': 'postgres', #Default username
+        'PASSWORD': '.j.XAWe:GF{2H^;2_',
+        'HOST': 'db', #name of docker container
+        'PORT': 5432 #postgresql default
     }
 }
 
@@ -121,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Custom User:
+
+AUTH_USER_MODEL = 'users.CustomUser'
---
+++ users/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+
+class CustomUserManager(BaseUserManager): 
+
+    #This function takes in as parameters only the required fields.
+    def create_user(self, email, password):
+        if not email: 
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email = self.normalize_email(email), #this makes all email lowercase to be stored
+        )
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+        
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password = password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+
+        return user
+
+
+#AbstractBaseUser provides core implementation
+#of user model, including hashed passwords and 
+#tokenized password resets.
+
+
+class CustomUser(AbstractBaseUser): 
+    #email field - we don't wsant username
+    email = models.EmailField(max_length=60, unique = True) 
+    
+    #Required fields for AbstractBaseUser
+    date_joined = models.DateTimeField(verbose_name = 'date joined', auto_now_add = True)
+    last_login = models.DateTimeField(verbose_name = "last login", auto_now = True)
+    is_admin = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+
+    #Set this to whatever you want the user to be able to login with
+    USERNAME_FIELD = 'email'
+    
+
+    #Set the user manager to the one created above.
+    objects = CustomUserManager()
+
+    def __str__(self): 
+        return self.email
+
+    def has_perm(self, perm, obj = None): #gets permissions for user
+        return self.is_admin
+    
+    def has_module_perms(self, app_label): #gets module permissions
+        return True
