c7f4d5a5ceac799e339bc95efb10983331982f86
DiegoloM2 <diego.moreno.manes@hotmail.com>
2022-06-08 17:46:27 +0200

    Fixed CreateTweetView and added all models in 'tweets' app to the admin site

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import NewsTweet, OpinionTweet
 # Register your models here.
+
+admin.site.register(NewsTweet)
+admin.site.register(OpinionTweet)
--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
 
 from .models import NewsTweet, OpinionTweet
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import CreateView
 
@@ -12,3 +10,24 @@ class NewsTweetCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateVie
     template_name = "tweets/createNewsTweet.html"
     login_url = "account_login"
     permission_required = "newstweets.create_permission"
+    success_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        form = self.get_form()
+        print(form)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        self.object = form.save(commit = False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super(NewsTweetCreateView, self).form_valid(form)
+
+
